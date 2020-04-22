28f94a04030a34d16abe1819e21601d269907286
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-22 18:07:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,16 +37,19 @@ class Song
       genre_count
   end
 
-  def self.artist_count
-    artist_count = {}
-    @atrists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
-  end
 
 end
+
+
+
+# def self.artist_count
+#   artist_count = {}
+#   @@atrists.each do |artist|
+#     if artist_count[artist]
+#       artist_count[artist] += 1
+#     else
+#       artist_count[artist] = 1
+#     end
+#   end
+#   artist_count
+# end
