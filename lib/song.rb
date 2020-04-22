28b6d771a04a650705544aad5de76e082f863a4a
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-22 17:34:35 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  @@song_count = 0
+  attr_accessor :name, :artist, :genre
+
+  def initialize
+    @@song_count += 1
+  end
+  
+  def self.count
+    @@song_count
+  end
+
+  
+  
+
+end
